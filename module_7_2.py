046dfe4f6aaf3965fd8753cab011076440183715
SyutkinMO <syutkin.mo@gmail.com>
2024-10-28 13:12:37 +0500

    1_2_3

--- module_7_2.py
+++ module_7_2.py
@@ -1,4 +1,4 @@
-#-------------------Позиционирование в файле-------------------
+# -------------------Позиционирование в файле-------------------
 
 # Создайте функцию custom_write(file_name, strings), которая принимает аргументы file_name
 # - название файла для записи, strings - список строк для записи.
@@ -7,7 +7,6 @@
 # а значением - записываемая строка.
 
 def custom_write(file_name, strings):
-
     base = {}
 
     file = open(f'{file_name}.txt', 'w+', encoding='utf-8')
@@ -24,22 +23,14 @@ def custom_write(file_name, strings):
 
     return base
 
+
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-    ]
+]
 
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
-
-
-
-# Создайте функцию custom_write(file_name, strings), которая принимает аргументы file_name
-# - название файла для записи, strings - список строк для записи.
-# Функция должна: Записывать в файл file_name все строки из списка strings, каждая на новой строке.
-# Возвращать словарь strings_positions, где ключом будет кортеж (<номер строки>, <байт начала строки>),
-# а значением - записываемая строка.
-
+    print(elem)
